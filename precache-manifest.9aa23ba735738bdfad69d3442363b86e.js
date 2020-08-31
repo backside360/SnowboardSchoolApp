da22b68956d3c1806c7acc2057bfa3d7ef328423
ovinazarov <ovinazarov@greenatom.ru>
2020-08-31 16:15:58 +0300

    Updates

---
+++ precache-manifest.9aa23ba735738bdfad69d3442363b86e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a885455a25c2f0f786ace45702f6a3e6",
+    "url": "/SnowboardSchoolApp/index.html"
+  },
+  {
+    "revision": "d423e6368b0b03cbee4a",
+    "url": "/SnowboardSchoolApp/static/css/2.847e01e0.chunk.css"
+  },
+  {
+    "revision": "a89eb63a523bf2c1d462",
+    "url": "/SnowboardSchoolApp/static/css/4.72d67c1c.chunk.css"
+  },
+  {
+    "revision": "6572df055782ebe3ee36",
+    "url": "/SnowboardSchoolApp/static/css/main.bf42586e.chunk.css"
+  },
+  {
+    "revision": "d423e6368b0b03cbee4a",
+    "url": "/SnowboardSchoolApp/static/js/2.c5098b90.chunk.js"
+  },
+  {
+    "revision": "8069250b617c26ffe172dee6b485360a",
+    "url": "/SnowboardSchoolApp/static/js/2.c5098b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae602c90ccb7c0cdc3c",
+    "url": "/SnowboardSchoolApp/static/js/3.fea7da24.chunk.js"
+  },
+  {
+    "revision": "a89eb63a523bf2c1d462",
+    "url": "/SnowboardSchoolApp/static/js/4.fac64cc8.chunk.js"
+  },
+  {
+    "revision": "2b795e2a683b18a00823",
+    "url": "/SnowboardSchoolApp/static/js/5.659bc88a.chunk.js"
+  },
+  {
+    "revision": "6572df055782ebe3ee36",
+    "url": "/SnowboardSchoolApp/static/js/main.870ea27c.chunk.js"
+  },
+  {
+    "revision": "7acc1e2fc317f4b7dcd7",
+    "url": "/SnowboardSchoolApp/static/js/runtime-main.ed6c3ffb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SnowboardS/precache-manifest.cc0b8b4d22c8bed814d790c97cbbff5a.js"
+  "/SnowboardSchoolApp/precache-manifest.9aa23ba735738bdfad69d3442363b86e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SnowboardS/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SnowboardSchoolApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
