ea3fa156d35a45acf01b40844799185bbdb317e3
ovinazarov <ovinazarov@greenatom.ru>
2020-08-31 16:01:48 +0300

    Updates

---
+++ static/js/5.659bc88a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsnowboard-app"]=this["webpackJsonpsnowboard-app"]||[]).push([[5],{311:function(e,n,t){"use strict";t.r(n);var a=t(0),r=t.n(a),o=t(126),c=t(125),u=t(124);n.default=function(e){return r.a.createElement(u.a,{header:function(){return r.a.createElement(o.a,{component:c.a})},body:function(){return r.a.createElement("h1",null,"News")}})}}}]);
+//# sourceMappingURL=5.659bc88a.chunk.js.map
